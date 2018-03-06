6d6fffde64384a6bc2f4e3f1d9bcdb15b25d190c
drdynscript <drdynscript@gmail.com>
2018-03-06 14:09:57 +0100

    Add proxy to react client

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null
+    }
+  }
+  componentDidMount() {
+    fetch('api/v1/users')
+      .then( response => response .json())
+      .then( item => this.setState({ user: item }));      
+  }
   render() {
+    let name = 'Alien';
+    if(this.state.user) {
+      name = this.state.user.data.name;
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome { name }</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- server/api/v1/controllers/userController.js
+++ server/api/v1/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
 exports.index = function(req, res) {
-  const user = new User({name: 'Philippe'});
+  const user = new User({name: 'Bol'});
   res.json(user);
 }
